7760b545e3a6db6dab7835bd110a1d21be023ff6
Patrick Marshall <marshallpt@vcu.edu>
2022-03-30 16:13:11 -0400

    Updated joy_launch to have namespace launch arg
    Added agent_name launch argument to joy_launch.
    
    joy_launch.py
    * re-factored to match our other launch files
    * added agent_name launch option

--- launch/joy_launch.py
+++ launch/joy_launch.py
@@ -1,17 +1,19 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
-    ld = LaunchDescription()
-
-    joy_node = Node(
-        package='joy',
-        namespace='car1',
-        name='joy_node',
-        executable='joy_node',
-    )
-
-    ld.add_action(joy_node)
-
-    return ld
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'agent_name',
+            default_value='car1',
+            description='Sets the namespace for this joy node.'),
+        Node(
+            package='joy',
+            namespace=[LaunchConfiguration('agent_name')],
+            name='joy_node',
+            executable='joy_node',
+        )
+    ])
